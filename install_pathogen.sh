91416af77408cd5fd2de54008d0075a22b3583f0
lmeru769 <nobody@marriott.com>
2018-03-06 13:05:50 -0500

    fixed install for pathogen

--- install_pathogen.sh
+++ install_pathogen.sh
@@ -1,2 +1,5 @@
+# Install Pathogen Package manager for VIM
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+git clone https://github.com/tpope/vim-pathogen
+cp vim-pathogen/autoload/pathogen.vim ~/.vim/autoload/
+rm -rf vim-pathogen
